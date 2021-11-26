812db6f851bd9bb6c9d08e3f1df648a6f36f7fd3
sarojvrc <saroj.vrc@gmail.com>
2021-11-26 16:03:42 +0530

    css added for App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css'
+import React, {useState, useContext} from 'react';
+import MainMenu from './Components/MainMenu';
+import Quiz from './Components/Quiz';
+import EndScreen from './Components/EndScreen';
+import {QuizContext} from "./Helpers/Contexts";
 
 function App() {
+
+  const [gameState, setGameState] = useState("menu");
+
   return (
     <div className="App">
       <h1>Quiz App</h1>
+
+      <QuizContext.Provider value={{gameState, setGameState}}>
+        {gameState === "menu" && <MainMenu />}
+        {gameState === "quiz" && <Quiz />}
+        {gameState === "endScreen" && <EndScreen />}
+      </QuizContext.Provider>
+
     </div>
   );
 }
