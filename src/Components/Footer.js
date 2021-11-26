72d2396909742b63b88056561c92e241a3672e2d
sarojvrc <saroj.vrc@gmail.com>
2021-11-26 18:02:22 +0530

    footer updated

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../App.css";
+
+function Footer() {
+    return (
+        <footer>
+            © Quiz App made by Saroj
+        </footer>
+    )
+}
+
+export default Footer
