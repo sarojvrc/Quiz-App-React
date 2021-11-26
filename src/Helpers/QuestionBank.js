cc73e286e882e058f0cac3f4248bc33d11163552
sarojvrc <saroj.vrc@gmail.com>
2021-11-26 16:43:15 +0530

    added questions

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt: "What is 1+3 ?",
+        optionA: "24",
+        optionB: "25",
+        optionC: "4",
+        optionD: "7",
+        answer: "C"
+    },
+    {
+        prompt: "Java is a ?",
+        optionA: "Language",
+        optionB: "Library",
+        optionC: "Framework",
+        optionD: "Module",
+        answer: "A"
+    },
+    {
+        prompt: "Age for Vote ?",
+        optionA: "24",
+        optionB: "25",
+        optionC: "4",
+        optionD: "18",
+        answer: "D"
+    },
+]
